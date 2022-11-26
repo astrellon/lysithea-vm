4828e68fe01bef7ca217f540fe5492cbbdb5c8b8
Astrellon <sovereign250@gmail.com>
2022-11-26 23:05:12 +1100

    Added some more exporting files for TS version

---
+++ ts/src/index.ts
@@ -0,0 +1,9 @@
+export * from "./virtualMachine";
+export * from "./assembler";
+export * from "./scope";
+export * from "./script";
+export * from "./vmFunction";
+export * from "./virtualMachineRunner";
+
+export * from "./values/index";
+export * from "./standardLibrary/index";
--- ts/src/standardLibrary/index.ts
+++ ts/src/standardLibrary/index.ts
@@ -5,6 +5,12 @@ import { arrayScope } from "./standardArrayLibrary";
 import { objectScope } from "./standardObjectLibrary";
 import { miscScope } from "./standardMiscLibrary";
 
+export { mathScope } from "./standardMathLibrary";
+export { stringScope } from "./standardStringLibrary";
+export { arrayScope } from "./standardArrayLibrary";
+export { objectScope } from "./standardObjectLibrary";
+export { miscScope } from "./standardMiscLibrary";
+
 export enum LibraryType {
     math = 1 << 0,
     string = 1 << 1,
